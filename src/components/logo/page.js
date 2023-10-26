459fb0ebd914d2f67d8caea7e9d00e22372c672e
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2023-10-25 22:41:25 -0300

    finished

--- src/components/logo/page.js
+++ src/components/logo/page.js
@@ -6,7 +6,7 @@ export function Logo() {
       <div className="flex items-center gap-2">
         <div className="rounded-full h-16 w-16 bg-gradient-to-b from-orange to-blue flex">
           <div className="rounded-full h-14 w-14 bg-bgLight dark:bg-bgDark m-auto flex">
-            <span className="text-5xl font-bold m-auto">G</span>
+            <span className="text-5xl font-bold m-auto">B</span>
           </div>
         </div>
 
